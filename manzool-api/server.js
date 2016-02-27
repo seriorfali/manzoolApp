3ba7175fc6f9516e29b504e3abf1a3373146f589
Seri <seriorfali@gmail.com>
2016-02-27 08:21:50 -0800

    Set up server listener, middleware for API routes.

--- manzool-api/server.js
+++ manzool-api/server.js
@@ -11,3 +11,46 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 // To log all requests in the console using the 'dev' syntax.
 app.use(logger("dev"))
+
+// API routes
+var clientRoutes = require("./routes/clientRoutes.js")
+  , imageRoutes = require("./routes/imageRoutes.js")
+  , projectRoutes = require("./routes/projectRoutes.js")
+  , showroomRoutes = require("./routes/showroomRoutes.js")
+  , userRoutes = require("./routes/userRoutes.js")
+  
+app.use("/api/v1/clients", clientRoutes)
+app.use("/api/v1/images", imageRoutes)
+app.use("/api/v1/projects", projectRoutes)
+app.use("/api/v1/showroom", showroomRoutes)
+app.use("/api/v1/users", userRoutes)
+
+var routeInfo = {
+	message: "Please submit a request to one of the available routes.",
+	get: {
+		
+	},
+	post: {
+		
+	},
+	put: {
+		
+	},
+	patch: {
+		
+	},
+	delete: {
+		
+	}
+}
+
+app.get("*", function(req, res) {
+	res.json(routeInfo)
+})
+
+// To run server
+var port = process.env.PORT || 3000
+
+app.listen(port, function() {
+	console.log("Server running on port " + port)
+})
